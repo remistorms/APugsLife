6d19428169a50a78dee0e6521f49b60add9ea80a
Rem Storms <ultimate.spidey.18@gmail.com>
2019-06-12 03:21:11 -0500

    Fixed Interactable

--- Assets/Scripts/Game/Interactable.cs
+++ Assets/Scripts/Game/Interactable.cs
@@ -2,8 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public abstract class Interactable : MonoBehaviour
+public class Interactable : MonoBehaviour
 {
+
     [Header("Basic Info")]
     public string interactableName;
 
@@ -21,26 +22,134 @@ public abstract class Interactable : MonoBehaviour
 
     [Header("Interactability Times")]
     [Range(0, 1)]
-    public float sniffPercentage;
-    protected float sniffCurrentTime;
-    public float sniffTimeNeeded;
+    public float sniffPercentage = 0;
+    protected float sniffCurrentTime = 0;
+    public float sniffTimeNeeded = 1;
     [Range(0, 1)]
-    public float barkPercentage;
-    protected float barkCurrentTime;
-    public float barkTimeNeeded;
+    public float barkPercentage = 0;
+    protected float barkCurrentTime = 0;
+    public float barkTimeNeeded = 1;
     [Range(0, 1)]
-    public float eatPercentage;
-    protected float eatCurrentTime;
-    public float eatTimeNeeded;
+    public float eatPercentage = 0;
+    protected float eatCurrentTime = 0;
+    public float eatTimeNeeded = 1;
     [Range(0, 1)]
-    public float markPercentage;
-    protected float markCurrentTime;
-    public float markTimeNeeded;
+    public float markPercentage = 0;
+    protected float markCurrentTime = 0;
+    public float markTimeNeeded = 1;
 
-    //Public Methods
-    public abstract void OnSniffed();
-    public abstract void OnBarked();
-    public abstract void OnEaten();
-    public abstract void OnMarked();
+    public void Update()
+    {
+        UpdateSniffState();
+        UpdateBarkState();
+        UpdateEatState();
+        UpdateMarkState();
+    }
 
+    //THIS UPDATEDS SLIDER VALUES
+    private void UpdateSniffState()
+    {
+        if (isBeingSniffed && hasBeenSniffed == false)
+        {
+            sniffCurrentTime += Time.deltaTime;
+            sniffPercentage = sniffCurrentTime / sniffTimeNeeded;
+
+            if (sniffCurrentTime >= sniffTimeNeeded)
+            {
+                sniffCurrentTime = sniffTimeNeeded;
+                hasBeenSniffed = true;
+                OnSniffedComplete();
+            }
+        }
+        else
+        {
+            sniffCurrentTime = 0;
+            sniffPercentage = 0;
+        }
+    }
+
+    private void UpdateBarkState()
+    {
+        if (isBeingBarked && hasBeenBarked == false)
+        {
+            barkCurrentTime += Time.deltaTime;
+            barkPercentage = barkCurrentTime / barkTimeNeeded;
+
+            if (barkCurrentTime >= barkTimeNeeded)
+            {
+                barkCurrentTime = barkTimeNeeded;
+                hasBeenBarked = true;
+                OnBarkedComplete();
+            }
+        }
+        else
+        {
+            barkCurrentTime = 0;
+            barkPercentage = 0;
+        }
+    }
+
+    private void UpdateMarkState()
+    {
+        if (isBeingMarked && hasBeenMarked == false)
+        {
+            markCurrentTime += Time.deltaTime;
+            markPercentage = markCurrentTime / markTimeNeeded;
+
+            if (markCurrentTime >= markTimeNeeded)
+            {
+                markCurrentTime = markTimeNeeded;
+                hasBeenMarked = true;
+                OnMarkedComplete();
+            }
+        }
+        else
+        {
+            markCurrentTime = 0;
+            markPercentage = 0;
+        }
+    }
+
+    private void UpdateEatState()
+    {
+        if (isBeingEaten && hasBeenEaten == false)
+        {
+            eatCurrentTime += Time.deltaTime;
+            eatPercentage = eatCurrentTime / eatTimeNeeded;
+
+            if (eatCurrentTime >= eatTimeNeeded)
+            {
+                eatCurrentTime = eatTimeNeeded;
+                hasBeenEaten = true;
+                OnEatenComplete();
+            }
+        }
+        else
+        {
+            eatCurrentTime = 0;
+            eatPercentage = 0;
+        }
+    }
+
+
+    //ON INTERACTED WITH
+    public void OnBarkedComplete()
+    {
+       // GetComponentInChildren<MeshRenderer>().material.color = Color.red;
+    }
+
+    public void OnEatenComplete()
+    {
+       // Destroy(this.gameObject);
+    }
+
+    public void OnMarkedComplete()
+    {
+        //GetComponentInChildren<MeshRenderer>().material.color = Color.green;
+    }
+
+    public void OnSniffedComplete()
+    {
+        //this.transform.localScale = new Vector3(2,2,2);
+    }
 }
